a72bedf7b3fd135fd85f8cd58d90c6e912c1cdb8
JC <jc@profitpeakmarketing.com>
2015-11-26 19:53:41 -0600

    commit

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/hello', function(request, response) {
+  response.render('pages/hello');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
