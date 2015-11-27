acb1037949dc241b555c1ccbb469cc4e2e833d3f
JC <jc@profitpeakmarketing.com>
2015-11-26 20:28:38 -0600

    add database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var pg = require('pg');
 var express = require('express');
 var app = express();
 
@@ -16,6 +17,20 @@ app.get('/', function(request, response) {
 app.get('/hello', function(request, response) {
   response.render('pages/hello');
 });
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
